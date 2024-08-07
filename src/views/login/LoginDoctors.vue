40fcc4b972588998725757873de4d8401a256d58
qsxl39 <2045872352@qq.com>
2024-08-07 12:57:54 +0800

    第一次登录页面上传
    未完成:部分样式,视口适配,路由,数据库

---
+++ src/views/login/LoginDoctors.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import Header from './components/Header.vue';
+import Button from "./components/Button.vue"
+</script>
+<template>
+  <div style="height: 130px; width: 100vw; background-color:#49998F;  line-height: 130px;">
+    <span style="color: aliceblue; font-size: 180%; padding-left: 10%;">
+      心理康复治疗平台
+    </span>
+    <div style="padding-top: 0;">
+      <span style="color: black; font-size: 140%; padding-left: 13%;">
+        <el-button color="#49998F" size="large" class="btn" plain>返回</el-button>
+      </span>
+    </div>
+    <!-- 按钮 -->
+    <div style="text-align: center; height: 100px;">账户名</div>
+    <div style="height: 0;width: 100vw; display: flex; align-items: center; justify-content: center;">
+      <input style="width: 360px;border: none; outline: none; border-bottom: 2px solid rgba(0, 0, 0, 0.4) ; "
+        placeholder="请输入账户名" clearable />
+    </div>
+    <div style="text-align: center; height: 100px;">密码</div>
+    <div style="height: 0;width: 100vw; display: flex; align-items: center; justify-content: center;">
+      <input style="width: 360px;border: none; outline: none; border-bottom: 2px solid rgba(0, 0, 0, 0.4) ; "
+        placeholder="请输入密码" clearable />
+    </div>
+    <div style="text-align: center; height: 100px; color: rgba(0, 0, 0, 0.4); font-size: 10px;">忘记密码请联系管理员</div>
+    <div style="height: 0;width: 100vw; display: flex; align-items: center; justify-content: center;">
+      <el-button color="#49998F" size="large" class="btn" plain>确定</el-button>
+    </div>
+  </div>
+</template>
+
+<style>
+.btn {
+  width: 200px;
+  height: 50px;
+  border-radius: 15px;
+  font-size: 22px;
+}
+</style>
--- src/views/login/LoginView.vue
+++ src/views/login/LoginView.vue
@@ -3,22 +3,31 @@ import Header from './components/Header.vue';
 import Button from "./components/Button.vue"
 </script>
 <template>
-  <Header />
-  <h2 class="tip">请选择你的角色</h2>
-  <div class="btnGroup">
-    <Button>医生</Button>
-    <Button>患者</Button>
+  <div style="height: 130px; width: 100vw; background-color:#49998F;  line-height: 130px;">
+    <span style="color: aliceblue; font-size: 180%; padding-left: 10%;">
+      心理康复治疗平台
+    </span>
+    <div style="padding-top: 0;">
+      <span style="color: black; font-size: 140%; padding-left: 13%;">
+        请选择您的角色
+      </span>
+    </div>
+      <div style="height: 160px;width: 100vw; display: flex; align-items: center; justify-content: center;">
+        <!-- 医生登录按钮 -->
+        <el-button style="  margin-right: 120px;" color="#49998F" size="large" class="btn" plain>医生</el-button>
+        <!-- 患者登录按钮 -->
+        <el-button style="  margin-left: 120px;" color="#49998F" size="large" class="btn" plain>患者</el-button>
+      </div>
   </div>
 </template>
 <style lang="scss" scoped>
 .tip{
   margin: 20px;
 }
-.btnGroup {
-  display: flex;
-  margin: auto;
-  justify-content: space-between;
-  width: 50%;
-  margin-top: 30vh;
+.btn {
+  width: 200px;
+  height: 50px;
+  border-radius: 15px;
+  font-size: 22px;
 }
 </style>
